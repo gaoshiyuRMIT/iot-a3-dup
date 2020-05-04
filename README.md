a77e6eb3d6783a20fc5b8f675d38fece8234095a
Shiyu <s3734720@student.rmit.edu.au>
2020-05-04 23:34:27 +1000

    MP web app startup script: print 2 flask app' exit status

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 ## MP Web App
 ### Usage
 - `./start_mp_web.sh`
+- the web site can be accessed at http://localhost:5000
+- the output from flask is redirected admin-app/flask.log and cloud-db-api/flask.log
 ### Deployment from Source Code
 - add cloud-db-api/config.py and admin-app/config.py
 - add admin-app/.flaskenv
--- start_mp_web.sh
+++ start_mp_web.sh
@@ -14,7 +14,7 @@ done
 
 if ( [ ! -d cloud-db-api/venv ] || [ ! -d admin-app/venv ] ) && [ -z $python ]
 then
-    echo "pls set environment viable $python. e.g. export python=python3.7"
+    echo "pls set environment variable $python. e.g. export python=python3.7"
     exit 1
 fi
 
@@ -43,6 +43,7 @@ else
 fi
 flask run --host=0.0.0.0 > flask.log 2>&1 &
 website_pid=$!
+deactivate
 echo "* admin-app successfully started"
 
 # echo "API PID: $api_pid, website PID: $website_pid"
@@ -50,5 +51,26 @@ echo "* admin-app successfully started"
 
 trap "kill $api_pid $website_pid" SIGINT
 
-wait $api_pid
-wait $website_pid
+api_exited=0
+website_exited=0
+while [ $api_exited -ne 1 ] || [ $website_exited -ne 1 ]
+do
+    if [ $api_exited -ne 1 ] && ! ps | grep "^$api_pid" > /dev/null
+    then
+        api_exited=1
+        if ! wait $api_pid
+        then
+            echo "* cloud-db-api exited with an error"
+        fi
+    fi
+    if [ $website_exited -ne 1 ] && ! ps | grep "^$website_pid" > /dev/null
+    then
+        website_exited=1
+        if ! wait $website_pid
+        then
+            echo "* admin-app exited with an error"
+        fi
+    fi
+    sleep 1
+done
+
