f95ba206b444ec4eaa097e8c602f71490837c0ec
Shiyu <s3734720@student.rmit.edu.au>
2020-05-09 14:04:28 +1000

    db integration - sql script to create tables

---
+++ cloud-db-api/sql/insertTestData.sql
@@ -0,0 +1,2 @@
+insert into Car (car_id, year, car_model, body_type, num_seats, car_colour, cost_hour, latitude, longitude, car_status) values 
+(1, 2010, 'Audi A6', "Sedan", 4, 'white', 2.0, -37, 144, 'available');
