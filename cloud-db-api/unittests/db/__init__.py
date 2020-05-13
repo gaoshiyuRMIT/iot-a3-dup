dd812302f79623166b76b38001d75286f57f1d28
Shiyu <s3734720@student.rmit.edu.au>
2020-05-13 15:51:31 +1000

    cloud-db-api unittests - car manager

---
+++ cloud-db-api/unittests/db/__init__.py
@@ -0,0 +1,11 @@
+import pymysql
+
+from unittest.mock import MagicMock, patch
+from unittests.test_config import test_config as cfg
+
+
+def patch_flask_g():
+    mock_g = MagicMock()
+    mock_g.__contains__.return_value = True
+    mock_g.conn = pymysql.connect(cfg["MYSQL_HOST"], cfg["MYSQL_USER"], cfg["MYSQL_PASSWORD"], cfg["MYSQL_DATABASE"])
+    return patch("flask.g", mock_g)
