9de5530683beb599dd68373f4aa586c895e978bb
duanxinhuan <duanxinhuan@163.com>
2020-06-13 15:39:26 +1000

    debug

--- ap/test.py
+++ ap/test.py
@@ -1,9 +1,10 @@
 import bluetooth
 from dataHelper import dataHelper
 from client import client
-c = client()
+
 helper = dataHelper()
 def send_and_valid(data):
+    c = client()
     """
     sending and valid MAC address
     :param string data: the sent data
@@ -12,6 +13,7 @@ def send_and_valid(data):
     """
     c.send_data(data)   
     status = c.listen_from_server() 
+    c.close_client()
     if status == 'success':
         return True
         
@@ -22,11 +24,12 @@ def main():
         if len(nearby_devices)>0:
             print('welcome, {}'.format(nearby_devices[0][1]))
             bd_address=nearby_devices[0][0]
-            data = dataHelper.validate_blue(bd_address)
-            if send_and_valid(data):
-                exec(open("/home/pi/Desktop/IotA3/iot/ap/engineer.py").read())
-                print('See u next time!')
-                quit()
+            print(bd_address)
+            data = helper.validate_blue(bd_address)
+            # if send_and_valid(data):
+            #     exec(open("/home/pi/Desktop/IotA3/iot/ap/engineer.py").read())
+            #     print('See u next time!')
+            quit()
         else:
             print('no device found!')    
 
