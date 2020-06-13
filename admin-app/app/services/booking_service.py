8510bc07915e734836b57dc8654eaf5fe5cd25dc
Shiyu <s3734720@student.rmit.edu.au>
2020-06-13 22:34:52 +1000

    docstrings for services

--- admin-app/app/services/booking_service.py
+++ admin-app/app/services/booking_service.py
@@ -5,21 +5,43 @@ class BookingService(BaseService):
     DATE_FMT = "%Y-%m-%d"
 
     def get_bookings_for_car(self, car_id: int):
+        '''get all bookings given car_id
+
+        :param int car_id: car ID
+        :return: all bookings of that car
+        :rtype: list
+        '''
         url = f"/bookings/search"
         data = {"car_id": car_id}
         bookings = self.post(url, data)
         return bookings
 
     def search_bookings(self, query: dict):
+        '''search for bookings given the query dictionary
+
+        :param dict query: specifies the exact values or range of a key to match
+        :return: bookings that satisfy the query
+        :rtype: list
+        '''
         url = f"/bookings/search"
         bookings = self.post(url, query)
         return bookings
 
     def transform_date(self, booking: dict):
+        '''given a booking, tranform the date_booking and date_return to a datetime.date object
+
+        :param dict booking: key-value map that represents a booking
+        '''
         booking["date_booking"] = datetime.strptime(booking["date_booking"], self.DATE_FMT).date()
         booking["date_return"] = datetime.strptime(booking["date_return"], self.DATE_FMT).date()
 
     def transform_time(self, booking: dict):
+        '''given a booking, tranform the time_booking and time_return to datetime.timedelta object
+
+        :param dict booking: key-value map that represents a booking
+        :return: booking after time-transformation
+        :rtype: dict
+        '''
         for k in ("time_booking", "time_return"):
             time_s = booking[k]
             h, m, s = [int(s_) for s_ in time_s.split(":")]
@@ -27,8 +49,19 @@ class BookingService(BaseService):
         return booking
 
     def gen_date_str(self, date_):
+        '''generate a string based on an app-specific format (see DATE_FMT) from a datetime.date object
+
+        :param datetime.date date_: input date
+        :return: date string that conforms to the app-specific format
+        :rtype: str
+        '''
         return date.strftime(date_, self.DATE_FMT)
 
     def get_all_with_cars(self):
+        '''get bookings along with car info
+        
+        :rtype: list
+        :return: all bookings, together with the car info of each booking
+        '''
         url = f"/bookings/all_with_cars"
         return self.get(url, {})
--- admin-app/app/services/user_service.py
+++ admin-app/app/services/user_service.py
@@ -4,6 +4,9 @@ from .base_service import BaseService
 class UserService(BaseService):
     def get_all_users(self):
         '''get all users
+
+        :return: all users
+        :rtype: list
         '''
         url = "/users/search"
         data = {}
@@ -11,40 +14,66 @@ class UserService(BaseService):
         return users
     
     def add_user(self, new_val: dict) -> bool:
+        '''add a new user
+
+        :param dict new_val: key-value map representing a new user
+        :return: whether the operation is successful
+        :rtype: bool
+        '''
         url = "/users/registerUser"
         data = self.post(url, new_val)
         return data["success"]
 
     def delete_user(self, username: str) -> bool:
+        '''given username, delete a user
+
+        :param str username: username of the user to delete
+        :return: whether the deletion is successful
+        :rtype: bool
+        '''
         url = f"/users/{quote(username)}"
         data = self.delete(url)
         return data['success']
 
     def update_user(self, username: str, new_val: dict) -> bool:
+        '''given username, update the user with new values
+
+        :param str username: username of the user to update
+        :param dict new_val: key-value map to update the user to be
+        :return: whether the operation is successful
+        :rtype: bool
+        '''
         url = f"/users/{quote(username)}/update"
         data = self.put(url, new_val)
         return data["success"]
 
     def get_activity_types(self):
+        '''get users' activity types & counts
+
+        :return: users' activities and counts, each item looks like {"activity": "XX", "count": x}
+        :rtype: list
+        '''
         url = f"/users/activity/types"
         return self.get(url, {})
-
-    def find_user(self, username):
-        '''establish whether user with <username>, return all user data or None 
-        if username does not exist
-        '''
-        url = f"/users/{quote(username)}"
-        data = self.get(url)
-        return data
     
     def search_users(self, query: dict):
+        '''search for users given a query dict
+
+        :param dict query: key-value/range to match users with
+        :return: all users that satisfy the query
+        :rtype: list
+        '''
         url = "/users/search"
         data = query
         cars = self.post(url, data)
         return cars
     
     def findExistingUser(self, username):
-        '''find a user with <username>, return user data or None if not exists
+        '''establish whether user with <username>, return all user data or None if username does not exist
+        
+        :param str username: username to look for
+        :return: user info
+        :rtype: dict
         '''
         url="/users/search"
         data = {'username': username }
