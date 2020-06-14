24e38c31cc28909f19418aea85adfc89ddadcd19
O_o <48227836+duanxinhuan@users.noreply.github.com>
2020-06-14 19:18:04 +1100

    fix qr bug

--- ap/engineer.py
+++ ap/engineer.py
@@ -8,7 +8,10 @@ def main():
         print("choose from the following options:\n1.scan QR code\n2.quit")
         option =input()
         if(option == '1'):
-            Q.scan_QR()
+            try:
+                Q.scan_QR()
+            except:
+                print("wrong input path ot invalid QR code")    
 
 if __name__ == "__main__":
     # execute only if run as a script
